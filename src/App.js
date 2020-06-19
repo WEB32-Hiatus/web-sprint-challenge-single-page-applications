16b7d8a90e67fd01a997a912d18d81b8c9075c82
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-06-19 14:41:36 -0700

    form almost done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Form from './Components/Form';
+import './App.css';
 
 const App = () => {
   return (
     <div className='app'>
-      <h1>Lambda Eats</h1>
+      <h1>Pizza Express</h1>
       <Form/>
     </div>
   );
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,20 +3,23 @@ import * as yup from 'yup';
 import axios from 'axios';
 
 const formSchema = yup.object().shape({
-    name: yup.string().required('Name is a required field')
+    name: yup
+    .string()
+    .required('Name is a required field'),
+    size: yup
+    .string()
+    .oneOf(['small', 'medium', 'large', 'x-Large'], 'Please select a size'),
 });
+
+const initialFormValues = {name: '', size: '', pepperoni: false, cheese: false, mushrooms: false, olives: false, instructions: ''}
 
 export default function Form() {
 
-    const [formState, setFormState] = useState({
-        name: '',
-        size: '',
-        toppings: '',
-        instructions: ''
-    });
+    const [formState, setFormState] = useState(initialFormValues)
 
     const [error, setError] = useState({
-        name: ''
+        name: '', 
+        size: ''
     });
 
     const [buttonDisabled, setButtonDisabled] = useState(true);
@@ -43,21 +46,26 @@ export default function Form() {
         .catch( (err) => {
             setError({
                 ...error,
-                [event.target.name]: err.error[0]
+                [event.target.name]: err.error
             });
         });
     };
 
     const formSubmit = (event) => {
         event.preventDefault();
-        axios.post('https://reqres.in/', formState).then( (response) => {
-            setOrders([...orders, response.data.name]);
+        axios
+        .post('https://reqres.in/', formState)
+        .then( (response) => {
+            setOrders([...orders, response.data]);
             console.log('success', orders);
 
             setFormState({
                 name: '',
                 size: '',
-                toppings: '',
+                pepperoni: '',
+                cheese: '',
+                mushrooms: '',
+                olives: '',
                 instructions: ''
             });
         })
@@ -71,7 +79,8 @@ export default function Form() {
 
         const newFormData = {
             ...formState, 
-            [event.target.name]: event.target.type === 'checkbox' ? event.target.checked : event.target.value
+            [event.target.name]: 
+                event.target.type === 'checkbox' ? event.target.checked : event.target.value
         };
         validateChange(event);
         setFormState(newFormData)
@@ -79,21 +88,53 @@ export default function Form() {
 
     return (
         <form onSubmit={formSubmit}>
-            <label htmlFor='name'>
-                Name
-                <input id='name' type='text' name='name' value={formState.name} onChange={inputChange}/>
-                {error.name.length > 2 ? <p className='error'>{error.name}</p> : null}
-            </label>
-            <label htmlFor='size'>
-                Size
-                <select id='size' type='text' name='size' value={formState.size} onChange={inputChange}>
-                <option value=''>Select an option</option>
-                <option value='Small'>Small</option>
-                <option value='Medium'>Medium</option>
-                <option value='Large'>Large</option>
-                <option value='X-Large'>X-Large</option>
-                </select>
-            </label>
+            <div className='Name'>
+                <label htmlFor='name'>
+                    <h3>Full Name</h3>
+                    <input type='text' name='name' value={formState.name} onChange={inputChange}/>
+                    {error.name.length > 0 ? <p className='error'>{error.name}</p> : null}
+                </label>
+            </div>
+            <br/>
+            <div className='Size'>
+                <label htmlFor='size'>
+                    <h3>Size</h3>
+                    <select name='size' value={formState.size} onChange={inputChange}>
+                    <option value=''>Select an option</option>
+                    <option value='small'>Small</option>
+                    <option value='medium'>Medium</option>
+                    <option value='large'>Large</option>
+                    <option value='x-Large'>X-Large</option>
+                    </select>
+                </label>
+            </div>
+            <br/>
+            <div className='Toppings'>
+                <label htmlFor='toppings'>
+                    <h3>Toppings:</h3>
+                    Pepperoni
+                    <input type='checkbox' name='pepperoni' checked={formState.pepperoni} onChange={inputChange}/>
+                    <br/>
+                    Cheese
+                    <input type='checkbox' name='cheese' checked={formState.cheese} onChange={inputChange}/>
+                    <br/>
+                    Mushrooms
+                    <input type='checkbox' name='mushrooms' checked={formState.mushrooms} onChange={inputChange}/>
+                    <br/>
+                    Olives
+                    <input type='checkbox' name='olives' checked={formState.olives} onChange={inputChange}/>
+                </label>
+            </div>
+            <br/>
+            <div className='Instructions'>
+                <label htmlFor='instructions'>
+                    <h3>Special Instructions</h3>
+                    <input type='text' name='instructions' value={formState.instructions} onChange={inputChange}/>
+                </label>
+            </div>
+            <br/>
+            <button disabled={buttonDisabled}>Submit</button>
+            <pre>{JSON.stringify(orders, null, 2)}</pre>
         </form>
     );
 };
